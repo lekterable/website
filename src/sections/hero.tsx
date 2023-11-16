fdb09afc6cfb9222c0a97b243c47aa3104878aed
Kornel Dubieniecki <lekterable@gmail.com>
2023-11-16 23:58:57 +0100

    fix: use safe height for hero section

--- src/sections/hero.tsx
+++ src/sections/hero.tsx
@@ -8,7 +8,7 @@ import HeroIMG from '~assets/hero.png'
 const Hero = () => (
   <section
     id="hero"
-    className="flex min-h-[100vh] flex-1 flex-col justify-between"
+    className="min-h-safe flex flex-1 flex-col justify-between"
   >
     <div className="mt-[89px] flex flex-1 px-[21px] sm:px-[47px] md:px-[55px] lg:px-[123px] xl:px-[144px]">
       <div className="relative mx-auto flex max-w-[322px] flex-1 flex-col justify-center text-[34px] leading-none tracking-wider xs:max-w-[377px] xs:text-[47px] sm:max-w-[521px] sm:text-[55px] md:max-w-[610px] md:text-[76px] lg:max-w-[843px] lg:text-[89px] xl:max-w-[1364px] xl:text-[123px] 2xl:text-[144px]">
